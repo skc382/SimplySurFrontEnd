39e3fff34fd125643142c6426166072aed7f9c82
skc382 <skc382@nyu.edu>
2017-03-28 21:53:55 -0500

    update bower dependencies

---
+++ src/app/galleryPage/gallery.controller.js
@@ -0,0 +1,13 @@
+(function () {
+    'use strict';
+
+    angular.module('ui.simply.sur').controller('GalleryController', GalleryController);
+
+    GalleryController.$inject=[];
+
+    function GalleryController() {
+        var vm = this;
+
+    }
+
+})();
---
+++ src/app/homePage/home.controller.js
@@ -0,0 +1,34 @@
+(function () {
+    'use strict';
+
+    angular.module('ui.simply.sur').controller('HomeController', HomeController);
+
+    function HomeController() {
+        var vm = this;
+
+        vm.myInterval = 50000;
+        vm.noWrapSlides = false;
+        vm.active = 0;
+        var slides = vm.slides = [];
+        var currIndex = 0;
+
+        init();
+
+        function addSlide(index) {
+            var newWidth = 600 + slides.length + 1;
+            slides.push({
+                image: '../resources/img/'+index+'.jpg',
+                text1: ['Nice image','Awesome photograph','That is so cool','I love that'][slides.length % 4],
+                text2: ['Wow!!','What the hell','Now thats what I call damn!','MMmmm mmMMm'][slides.length % 4],
+                id: currIndex++
+            });
+        };
+
+        function init() {
+            for (var i = 1; i < 4; i++) {
+                addSlide(i);
+            }
+        }
+    }
+
+})();
