f6bef6c3aae2e0ddd201a77aaddaa334c5e3d582
Shreedhar KC <shreedharkempanayakanahallichampakaram@Shreedhars-MacBook-Pro.local>
2017-06-02 23:54:37 -0500

    add feature to home-page

--- src/app/home-page/home.controller.js
+++ src/app/home-page/home.controller.js
@@ -3,7 +3,9 @@
 
     angular.module('ui.simply.sur').controller('HomeController', HomeController);
 
-    function HomeController() {
+    HomeController.$inject = ['$window', '$state'];
+
+    function HomeController($window, $state) {
         var vm = this;
 
         vm.myInterval = 1;
@@ -17,14 +19,26 @@
         function addSlide(index) {
             var newWidth = 600 + slides.length + 1;
             slides.push({
-                image: '../resources/img/'+index+'.jpg',
-                text1: ['Nice image','','Very Cool',''][slides.length % 4],
-                text2: ['Wow!!','','',''][slides.length % 4],
+                image: getBasePath() + index + '.jpg',
+                text1: ['Nice image', '', 'Very Cool', ''][slides.length % 4],
+                text2: ['Wow!!', '', '', ''][slides.length % 4],
                 id: currIndex++
             });
         };
 
+
+        function getBasePath() {
+            if ($window.innerWidth < 500) {
+                return '../resources/img/lessthan500/';
+            }
+            return '../resources/img/';
+        }
+
         function init() {
+            angular.element($window).on('resize', function () {
+                $state.reload();
+            });
+
             for (var i = 9; i < 12; i++) {
                 addSlide(i);
             }
