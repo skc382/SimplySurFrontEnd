e577bf938fd1b81083d89d3c195c6720f89c6052
Shreedhar KC <skc382@nyu.edu>
2017-06-18 10:17:09 -0500

    add portfolio page and some style changes

---
+++ src/app/portfolio-page/portfolio.controller.js
@@ -0,0 +1,11 @@
+(function () {
+    'use strict';
+
+    angular.module('ui.simply.sur').controller('PortfolioController', PortfolioController);
+
+    PortfolioController.$inject=['intro'];
+
+    function PortfolioController() {
+        var vm = this;
+    }
+})();
