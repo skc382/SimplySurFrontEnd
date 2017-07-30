19d1003caa0a78af4c0b2cfe27f9afaaa820b511
Shreedhar KC <skc382@nyu.edu>
2017-07-30 17:41:42 -0500

    feat: minor changes

--- src/app/events-page/events.controller.js
+++ src/app/events-page/events.controller.js
@@ -16,40 +16,7 @@
                 },
                 'time': '07:30 am',
                 'details': {
-                    'title': 'Event One',
-                    'description': 'A wide variaety of food will be available for the guests to indulge in. Our School children will preform '
-                    +'various styles of dance and singing. A prize will be given out for the winner. ' +
-                    'various styles of dance and singing. A prize will be given out for the winner' +
-                    'various styles of dance and singing. A prize will be given out for the winner' +
-                    'various styles of dance and singing. A prize will be given out for the winner' +
-                    'various styles of dance and singing. A prize will be given out for the winner' +
-                    'various styles of dance and singing. A prize will be given out for the winner'
-                }
-            },
-            {
-                'date': {
-                    'day': 14,
-                    'month': 'Sep',
-                    'Year': 2017,
-
-                },
-                'time': '01:30 pm',
-                'details': {
-                    'title': 'Event two',
-                    'description': 'A wide variaety of food will be available for the guests to indulge in. Our School children will preform '
-                    +'various styles of dance and singing. A prize will be given out for the winner'
-                }
-            },
-            {
-                'date': {
-                    'day': 18,
-                    'month': 'May',
-                    'Year': 2016,
-
-                },
-                'time': '11:30 am',
-                'details': {
-                    'title': 'Event three',
+                    'title': 'Ram Leela',
                     'description': 'A wide variaety of food will be available for the guests to indulge in. Our School children will preform '
                     +'various styles of dance and singing. A prize will be given out for the winner. ' +
                     'various styles of dance and singing. A prize will be given out for the winner' +
